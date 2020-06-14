0b198cad97de8e7468f75f46b46231ad4f9ad813
Chris Arnoult <chris.arnoult@icloud.com>
2020-06-14 11:32:24 -0700

    initial commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Chris on 6/14/20.
+//  Copyright © 2020 Chris Arnoult. All rights reserved.
+//
+
+import SwiftUI
+
+// This is functional programming. This ConentView "behaves" or "functions" like a View. Not "is a" view. That's OOP.
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!!!!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
