ca24f61961c1170c5dcb3beec068955250feb4d4
Chris Arnoult <chris.arnoult@icloud.com>
2020-06-14 12:11:52 -0700

    Update ContentView.swift
    ContentView now displays 4 "CardViews" with an orange boarder around them and padding around the entire HStack.
    
    Created struct CardView to show what each card will look like when face up or face down.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,9 +11,44 @@ import SwiftUI
 // This is functional programming. This ConentView "behaves" or "functions" like a View. Not "is a" view. That's OOP.
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!!!!!")
+        HStack {
+            ForEach(0..<4) { i in
+                CardView(isFaceUp: true)
+            }
+        }
+            .foregroundColor(Color.orange)
+            .padding()
+            .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
